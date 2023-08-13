e4f50bac72691b13050be03d91814868ca65e93e
kottakota chaitanya <136222639+chaitnaya0119@users.noreply.github.com>
2023-08-13 15:18:28 +0530

    Add files via upload

---
+++ get line.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cout<< "Enter the name of the person: \n";
+    cin>>name ;
+    cout<< " Hi "<<name;
+    return 0;
+}
